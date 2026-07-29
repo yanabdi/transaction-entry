f48495c434818523792e70e2874c17d8f28bff0c
yanabdi <yanabdi12@gmail.com>
2026-07-29 15:31:50 -0700

    app will now run a localhost server instead

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,12 @@
-class App {
-    constructor() {
-        console.log("Welcome to Transaction Records");
-        console.log("1: Add Transaction");
-    }
-}
+import express from "express";
 
-const app = new App();
+const app = express();
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+    res.send("App Starting...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
