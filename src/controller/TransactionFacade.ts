4b367dd8c0211552a80e11a6cfb9d5d0751f3e90
yanabdi <yanabdi12@gmail.com>
2026-07-31 11:15:17 -0700

    started refactor to address duplicate transactions/duplicate dates problem

--- src/controller/TransactionFacade.ts
+++ src/controller/TransactionFacade.ts
@@ -2,21 +2,28 @@ import { TransactionRecord } from "../model/TransactionRecord.js";
 import { TTransactionFacade } from "./TTransactionFacade.js";
 
 export default class TransactionFacade implements TTransactionFacade {
-    private transactions: Map<number, TransactionRecord> = new Map();
+    private transactions: Map<number, Array<TransactionRecord>> = new Map();
 
     public async addTransaction(id: number, transaction: TransactionRecord): Promise<TransactionRecord> {
-         if (this.transactions.has(id)) {
-            throw new Error("Transaction already exists");
+        let value;
+        if (this.transactions.has(id)) {
+            value = this.transactions.get(id);
+            value.push(transaction);
+        } else {
+            this.transactions.set(id, new Array<TransactionRecord>());
+            value = this.transactions.get(id);
+            value.push(transaction);
         }
 
-        this.transactions.set(id, transaction);
+        // !!! NEED TO HANDLE DUPLICATE TRANSACTIONS OR AT LEAST FLAG IT OUT
 
         return transaction;
     }
 
+    // !!! NEEDS TO BE REFACTORED BELOW
     public async removeTransaction(id: number): Promise<string> {
         const deleted = this.transactions.delete(id);
-          if (!deleted) {
+        if (!deleted) {
             throw new Error("Transaction not found");
         }
 
