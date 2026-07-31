d24781ec33f8b0baf8fa1b416a9840357a181f08
yanabdi <yanabdi12@gmail.com>
2026-07-31 16:53:30 -0700

    refactored the code to handle an infinite amount of transactions in each date (by day); please view issue #15 for design issue with this

--- src/controller/TransactionFacade.ts
+++ src/controller/TransactionFacade.ts
@@ -5,32 +5,42 @@ export default class TransactionFacade implements TTransactionFacade {
     private transactions: Map<number, Array<TransactionRecord>> = new Map();
 
     public async addTransaction(id: number, transaction: TransactionRecord): Promise<TransactionRecord> {
-        let value;
         if (this.transactions.has(id)) {
-            value = this.transactions.get(id);
-            value.push(transaction);
-        } else {
-            this.transactions.set(id, new Array<TransactionRecord>());
-            value = this.transactions.get(id);
-            value.push(transaction);
+            let dayOfTransactions = this.transactions.get(id);
+            if (dayOfTransactions === undefined) {
+                let array = new Array<TransactionRecord>();
+                this.transactions.set(id, array);
+                transaction.entry = 1;
+                array.push(transaction);
+            } else {
+                let lastAdded = dayOfTransactions[dayOfTransactions.length - 1];
+                transaction.entry = lastAdded.entry + 1;
+                dayOfTransactions.push(transaction);
+            }
         }
-
-        // !!! NEED TO HANDLE DUPLICATE TRANSACTIONS OR AT LEAST FLAG IT OUT
 
         return transaction;
     }
 
-    // !!! NEEDS TO BE REFACTORED BELOW
-    public async removeTransaction(id: number): Promise<string> {
-        const deleted = this.transactions.delete(id);
-        if (!deleted) {
-            throw new Error("Transaction not found");
+    public async removeTransaction(id: number, entry: number): Promise<string> {
+        const dayOfTransactions = this.transactions.get(id);
+        if (!dayOfTransactions) {
+            throw new Error("Day not found.");
+        }
+        if (dayOfTransactions.length === 0) {
+            throw new Error("No transactions found for this day.");
+        }
+        for (const transaction of dayOfTransactions) {
+            if (transaction.entry === entry) {
+                dayOfTransactions.splice(dayOfTransactions.indexOf(transaction), 1);
+                return `Transaction entry: ${entry} deleted`;
+            }
         }
 
-        return `Transaction ${id} deleted`;
+        return `Unsuccessful deletion. Transaction entry: ${entry} not found.`;
     }
 
-    listTransactions(): TransactionRecord[] {
+    listTransactions(): TransactionRecord[][] {
         return Array.from(this.transactions.values());
     }
 }
