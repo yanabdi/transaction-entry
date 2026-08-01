273ad6d291a2e4896352add5664fba63cd32da54
ketchupspill <sean.sun231@gmail.com>
2026-07-31 19:14:15 -0700

    fixed bug in addTransactions

--- src/controller/TransactionFacade.ts
+++ src/controller/TransactionFacade.ts
@@ -5,19 +5,38 @@ export default class TransactionFacade implements TTransactionFacade {
     private transactions: Map<number, Array<TransactionRecord>> = new Map();
 
     public async addTransaction(id: number, transaction: TransactionRecord): Promise<TransactionRecord> {
-        if (this.transactions.has(id)) {
-            let dayOfTransactions = this.transactions.get(id);
-            if (dayOfTransactions === undefined) {
-                let array = new Array<TransactionRecord>();
-                this.transactions.set(id, array);
-                transaction.entry = 1;
-                array.push(transaction);
-            } else {
-                let lastAdded = dayOfTransactions[dayOfTransactions.length - 1];
-                transaction.entry = lastAdded.entry + 1;
-                dayOfTransactions.push(transaction);
-            }
+        let dayOfTransactions = this.transactions.get(id);
+
+        if (dayOfTransactions === undefined) {
+            dayOfTransactions = [];
+
+            transaction.entry = 1;
+
+            dayOfTransactions.push(transaction);
+
+            this.transactions.set(id, dayOfTransactions);
         }
+        else {
+            const lastTransaction = dayOfTransactions[dayOfTransactions.length - 1];
+
+            transaction.entry = lastTransaction.entry + 1;
+
+            dayOfTransactions.push(transaction);
+        } 
+        // below is old version, originally if map didnt contain ID array creation would not happen.
+        // if (this.transactions.has(id)) { let dayOfTransactions = this.transactions.get(id);
+        //     let dayOfTransactions = this.transactions.get(id);
+        //     if (dayOfTransactions === undefined) {
+        //         let array = new Array<TransactionRecord>();
+        //         this.transactions.set(id, array);
+        //         transaction.entry = 1;
+        //         array.push(transaction);
+        //     } else {
+        //         let lastAdded = dayOfTransactions[dayOfTransactions.length - 1];
+        //         transaction.entry = lastAdded.entry + 1;
+        //         dayOfTransactions.push(transaction);
+        //     }
+        // }
 
         return transaction;
     }
