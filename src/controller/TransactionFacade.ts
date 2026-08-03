85e5a9601677d1dfaadedb36a6b5e99f6b834d61
yanabdi <yanabdi12@gmail.com>
2026-08-02 18:22:54 -0700

    added @ts-ignore to TransactionFacade to take out the squiggly lines for now

--- src/controller/TransactionFacade.ts
+++ src/controller/TransactionFacade.ts
@@ -5,12 +5,14 @@ export class TransactionFacade implements TTransaactionFacade {
     private transactions: Map<number, TransactionRecord> = new Map<number, TransactionRecord>;
 
     async addTransaction(id: number, transaction: TransactionRecord): Promise<Map<number, TransactionRecord>> {
+        // @ts-ignore
         return;
-    }
+        }
 
     async removeTransaction(id: number): Promise<string> {
+        // @ts-ignore
         return;
-    }
+        }
 
     listTransactions() {
         
